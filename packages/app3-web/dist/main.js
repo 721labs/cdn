c42d3e6ee958ac346668035d1f0ca28215e4c00f
Will Holley <will@willholley.com>
2022-03-02 09:14:19 -0500

    Removes all app3-web files

--- packages/app3-web/dist/main.js
+++
@@ -1,3 +0,0 @@
-// v: 1311033c691475a7d35ef6ddf84e98e388d4b24a
-// 721.dev
-(()=>{window.app3=window.app3||{};})();
--- packages/app3-web/main.js
+++
@@ -1,3 +0,0 @@
-// v: 3bb02ce63508f67816f5af931b07bace08d0a7b8
-// 721.dev
-(()=>{console.log("App3 Test");})();
